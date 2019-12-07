94c5c9009303fbac520eecb46347b1692de804f4
niclin <bboyceo@hotmail.com>
2019-12-07 08:56:26 +0800

    Implement taipei city json

--- server/app.js
+++ server/app.js
@@ -5,6 +5,18 @@ app.get('/', function (req, res) {
   res.send('GET request to the homepage');
 });
 
+app.get('/weather/api/weathers/1.json', function(req, res) {
+  fake_data = {
+    data: {
+      felt_air_temp: 100,
+      rainfall: 1,
+      desc: "Good"
+    }
+  }
+
+  res.send(JSON.stringify(fake_data));
+})
+
 app.listen(3000, function () {
   console.log("HTTP server is listening at port 3000.")
 });
